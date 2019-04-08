6dc0d8fb45d35c662aeebcc1eacde6f54560bc0f
CinthiaVilcachagua <cv.ccori@gmail.com>
2019-04-08 16:23:10 -0500

    añadiendo las funciones de reconocer si es un directorio o un archivo.

--- src/path.js
+++ src/path.js
@@ -1,11 +1,27 @@
 const path = require('path');
+const fs = require('fs');
+//ejemplos unitarios
 console.log(path.isAbsolute('/test/demo_path.js'));
 console.log(path.isAbsolute('test/demo_path.js'));
 console.log(path.resolve('test/demo_path.js'));
 
+
+// -- ARMANDO LAS FUNCIONES (reconocer y convertir) --
+const pathIsAbs = (paths) => {
+    path.isAbsolute(paths) //return boolean
+}
+const convertToAbs = (paths) => {
+    path.resolve(paths) //return path Absolut
+}
+
+//una condicional
 const mdLinks = (ruta) => {  
     if(path.isAbsolute(ruta) === false){
         return console.log(path.resolve(ruta));
     }
 }
 mdLinks('test/demo_path.js');
+
+// -- FUNCIONES (Reconociendo archivos o directorios) --
+const validateAFile = (ruta) => {fs.lstatSync(ruta).isFile ()} //return boolean
+const validateAdirectory = (ruta) => {fs.lstatSync(ruta).isDirectory()} //return boolean
