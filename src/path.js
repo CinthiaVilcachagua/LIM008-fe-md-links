d883592b5b7d627fd91367be358453b46d86bec9
CinthiaVilcachagua <cv.ccori@gmail.com>
2019-04-11 19:00:05 -0500

    añadiendo la función extractionMdFiles, que me premite extraer los archivos '.m' de un directorio o ya sea un archivo propio insertandolos todo en un array vacio.

--- src/path.js
+++ src/path.js
@@ -5,37 +5,26 @@ const fs = require('fs');
 export const pathIsAbs = paths => path.isAbsolute(paths) //return boolean
 export const convertToAbs = paths => path.resolve(paths) //return path Absolut
 
-/*
-//una condicional
-const mdLinks = (path) => {  
-    if(pathIsAbs(path) === false){
-        return console.log(convertToAbs(path));
-    }
-}
-mdLinks('test/demo_path.js'); */
-
 // -- FUNCIONES (Reconociendo archivos o directorios y extension '.md') --
 export const validateAFile = ruta => fs.statSync(ruta).isFile() //return boolean
 export const validateADirectory = ruta => fs.statSync(ruta).isDirectory() //return boolean
 export const isAFileMd = paths => path.extname (paths) === '.md' //return boolean
-/*
-const pathsExtractionDirectory = (ruta) => {
-    if(validateADirectory(ruta) === 'true'){
-        //codigo
-    } else {
-        pathsExtractionFile(ruta)
-    }
-} 
 
-const pathsExtractionFile = (path) => {
-    if(validateAFile(path) === 'true'){
-        if(isAFileMd === 'true'){
-            //codigo
-        }
-        else {
-            END //remplazar por terminar.
-        }
-    } else {
-        END //remplazar por terminar.
-    }
-} */
+  const extractionMdFiles = (paths) => {
+    let arrayPaths = [];
+    //if(validateADirectory(paths) === true){
+        const arrayNameFiles = fs.readdirSync(paths);
+        console.log(arrayNameFiles)
+        arrayNameFiles.forEach(element => {
+            let pathToFile = path.join(paths, element);
+            console.log(pathToFile);
+            if (validateAFile(pathToFile) === true && isAFileMd(pathToFile) === true) {
+            arrayPaths.push(pathToFile);
+            } else if (validateADirectory(pathToFile) === true) {  //validateADirectory(pathToFile) === true
+                arrayPaths = arrayPaths.concat(extractionMdFiles(pathToFile));
+                console.log(arrayPaths)
+            }
+        })
+    return arrayPaths;
+  } 
+  console.log(extractionMdFiles("C:\\Users\\Laboratoria\\Documents\\Laboratoria\\Proyecto 4\\LIM008-fe-md-links\\test\\Prueba\\nodo 2"))
